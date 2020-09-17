2592d2b5da545eeb4449f2e9a7b15de319ec03ab
Lukas Schwab <lukas.schwab@lukass-mbp.home>
2020-09-17 16:13:41 +0200

    fix jq command

--- .circleci/helpers/setEnvironmentVariables.sh
+++ .circleci/helpers/setEnvironmentVariables.sh
@@ -3,26 +3,18 @@ set -eu
 
 cd ~/project/config
 
-jq --arg pk "$DIALOGFLOW_CLIENT_EMAIL_DEV" '.DIALOGFLOW_CLIENT_EMAIL_DEV = $pk' config.dev.json > config.dev.json.tmp \
-    && mv config.dev.json.tmp config.dev.json
+jq --arg pk "$DIALOGFLOW_CLIENT_EMAIL_DEV" '.DIALOGFLOW_CLIENT_EMAIL_DEV = $pk' config.dev.json
 
-jq --arg pk "$DIALOGFLOW_PRIVATE_KEY_DEV" '.DIALOGFLOW_PRIVATE_KEY_DEV = $pk' config.dev.json > config.dev.json.tmp \
-    && mv config.dev.json.tmp config.dev.json
+jq --arg pk "$DIALOGFLOW_PRIVATE_KEY_DEV" '.DIALOGFLOW_PRIVATE_KEY_DEV = $pk' config.dev.json
 
-jq --arg pk "$DIALOGFLOW_PROJECT_ID_DEV" '.DIALOGFLOW_PROJECT_ID_DEV = $pk' config.dev.json > config.dev.json.tmp \
-    && mv config.dev.json.tmp config.dev.json
+jq --arg pk "$DIALOGFLOW_PROJECT_ID_DEV" '.DIALOGFLOW_PROJECT_ID_DEV = $pk' config.dev.json
 
-jq --arg pk "$SLACK_BOT_TOKEN_DEV" '.SLACK_BOT_TOKEN_DEV = $pk' config.dev.json > config.dev.json.tmp \
-    && mv config.dev.json.tmp config.dev.json
+jq --arg pk "$SLACK_BOT_TOKEN_DEV" '.SLACK_BOT_TOKEN_DEV = $pk' config.dev.json
 
-jq --arg pk "$DIALOGFLOW_CLIENT_EMAIL_PROD" '.DIALOGFLOW_CLIENT_EMAIL_PROD = $pk' config.prod.json > config.prod.json.tmp \
-    && mv config.prod.json.tmp config.prod.json
+jq --arg pk "$DIALOGFLOW_CLIENT_EMAIL_PROD" '.DIALOGFLOW_CLIENT_EMAIL_PROD = $pk' config.prod.json
 
-jq --arg pk "$DIALOGFLOW_PRIVATE_KEY_PROD" '.DIALOGFLOW_PRIVATE_KEY_PROD = $pk' config.prod.json > config.prod.json.tmp \
-    && mv config.prod.json.tmp config.prod.json
+jq --arg pk "$DIALOGFLOW_PRIVATE_KEY_PROD" '.DIALOGFLOW_PRIVATE_KEY_PROD = $pk' config.prod.json
 
-jq --arg pk "$DIALOGFLOW_PROJECT_ID_PROD" '.DIALOGFLOW_PROJECT_ID_PROD = $pk' config.prod.json > config.prod.json.tmp \
-    && mv config.prod.json.tmp config.prod.json
+jq --arg pk "$DIALOGFLOW_PROJECT_ID_PROD" '.DIALOGFLOW_PROJECT_ID_PROD = $pk' config.prod.json
 
-jq --arg pk "$SLACK_BOT_TOKEN_PROD" '.SLACK_BOT_TOKEN_PROD = $pk' config.prod.json > config.dev.prod.tmp \
-    && mv config.prod.json.tmp config.prod.json
+jq --arg pk "$SLACK_BOT_TOKEN_PROD" '.SLACK_BOT_TOKEN_PROD = $pk' config.prod.json
