0164a94ff16b0ad37626048ee190598d74897412
Lukas Schwab <lukas.schwab@Lukass-MacBook-Pro.local>
2020-09-24 16:50:31 +0200

    logging

--- handler.ts
+++ handler.ts
@@ -5,6 +5,9 @@ import { errorHandler } from './src/handlers/errorHandler';
 
 export const slackevent = (event: { body: string }, context: Context): HandlerResponse => {
 	if (JSON.parse(event.body).type === 'url_verification') {
+		console.log('url_verification event');
+		console.log(JSON.stringify(event.body));
+
 		return {
 			statusCode: 200,
 			headers: {
@@ -13,22 +16,22 @@ export const slackevent = (event: { body: string }, context: Context): HandlerRe
 			body: '',
 			challenge: JSON.parse(event.body).challenge,
 		};
-	}
+	} else {
+		try {
+			context.callbackWaitsForEmptyEventLoop = false;
+			console.log(`Received event: ${JSON.stringify(event)}`);
+			const slackEvent = JSON.parse(event.body) as ISlackMessageIMEvent;
+			slackMessageIMHandler(slackEvent);
+		} catch (err) {
+			errorHandler(err, err.message);
+		}
 
-	try {
-		context.callbackWaitsForEmptyEventLoop = false;
-		console.log(`Received event: ${JSON.stringify(event)}`);
-		const slackEvent = JSON.parse(event.body) as ISlackMessageIMEvent;
-		slackMessageIMHandler(slackEvent);
-	} catch (err) {
-		errorHandler(err, err.message);
+		return {
+			statusCode: 200,
+			body: '',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		};
 	}
-
-	return {
-		statusCode: 200,
-		body: '',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-	};
 };
