b03b4768987e77f91fa851e7abf817068e373e76
Lukas Schwab <lukas.schwab@Lukass-MacBook-Pro.local>
2020-09-24 12:57:01 +0200

    Fix environment variables

--- .circleci/helpers/setEnvironmentVariables.sh
+++ .circleci/helpers/setEnvironmentVariables.sh
@@ -3,18 +3,18 @@ set -eu
 
 cd ~/project/config
 
-jq --arg pk "$DIALOGFLOW_CLIENT_EMAIL_DEV" '.DIALOGFLOW_CLIENT_EMAIL_DEV = $pk' config.dev.json
+jq --arg dced "$DIALOGFLOW_CLIENT_EMAIL_DEV" '.DIALOGFLOW_CLIENT_EMAIL_DEV = $dced' config.dev.json
 
-jq --arg pk "$DIALOGFLOW_PRIVATE_KEY_DEV" '.DIALOGFLOW_PRIVATE_KEY_DEV = $pk' config.dev.json
+jq --arg dpkd "$DIALOGFLOW_PRIVATE_KEY_DEV" '.DIALOGFLOW_PRIVATE_KEY_DEV = $dpkd' config.dev.json
 
-jq --arg pk "$DIALOGFLOW_PROJECT_ID_DEV" '.DIALOGFLOW_PROJECT_ID_DEV = $pk' config.dev.json
+jq --arg dpid "$DIALOGFLOW_PROJECT_ID_DEV" '.DIALOGFLOW_PROJECT_ID_DEV = $dpid' config.dev.json
 
-jq --arg pk "$SLACK_BOT_TOKEN_DEV" '.SLACK_BOT_TOKEN_DEV = $pk' config.dev.json
+jq --arg sbtd "$SLACK_BOT_TOKEN_DEV" '.SLACK_BOT_TOKEN_DEV = $sbtd' config.dev.json
 
-jq --arg pk "$DIALOGFLOW_CLIENT_EMAIL_PROD" '.DIALOGFLOW_CLIENT_EMAIL_PROD = $pk' config.prod.json
+jq --arg dcep "$DIALOGFLOW_CLIENT_EMAIL_PROD" '.DIALOGFLOW_CLIENT_EMAIL_PROD = $dcep' config.prod.json
 
-jq --arg pk "$DIALOGFLOW_PRIVATE_KEY_PROD" '.DIALOGFLOW_PRIVATE_KEY_PROD = $pk' config.prod.json
+jq --arg dpkp "$DIALOGFLOW_PRIVATE_KEY_PROD" '.DIALOGFLOW_PRIVATE_KEY_PROD = $dpkp' config.prod.json
 
-jq --arg pk "$DIALOGFLOW_PROJECT_ID_PROD" '.DIALOGFLOW_PROJECT_ID_PROD = $pk' config.prod.json
+jq --arg dpip "$DIALOGFLOW_PROJECT_ID_PROD" '.DIALOGFLOW_PROJECT_ID_PROD = $dpip' config.prod.json
 
-jq --arg pk "$SLACK_BOT_TOKEN_PROD" '.SLACK_BOT_TOKEN_PROD = $pk' config.prod.json
+jq --arg sbtp "$SLACK_BOT_TOKEN_PROD" '.SLACK_BOT_TOKEN_PROD = $sbtp' config.prod.json
