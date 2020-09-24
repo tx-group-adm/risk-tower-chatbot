3c7afde91544ad2cc93281c4b4cd8627cf800b1e
Lukas Schwab <lukas.schwab@Lukass-MacBook-Pro.local>
2020-09-24 15:39:46 +0200

    fix bash script

--- .circleci/helpers/setEnvironmentVariables.sh
+++ .circleci/helpers/setEnvironmentVariables.sh
@@ -5,13 +5,13 @@ cd ~/project/config
 
 jq --arg pk "$DIALOGFLOW_PRIVATE_KEY_DEV" '.DIALOGFLOW_PRIVATE_KEY_DEV = $pk' config.dev.json
 
-sed -i 's~###DIALOGFLOW_CLIENT_EMAIL###~"$DIALOGFLOW_CLIENT_EMAIL_DEV"~' config.dev.json
-sed -i 's~###DIALOGFLOW_PROJECT_ID###~"$DIALOGFLOW_PROJECT_ID_DEV"~' config.dev.json
-sed -i 's~###SLACK_BOT_TOKEN###~"$SLACK_BOT_TOKEN_DEV"~' config.dev.json
+sed -i "s~###DIALOGFLOW_CLIENT_EMAIL###~${DIALOGFLOW_CLIENT_EMAIL_DEV}~" config.dev.json
+sed -i "s~###DIALOGFLOW_PROJECT_ID###~${DIALOGFLOW_PROJECT_ID_DEV}~" config.dev.json
+sed -i "s~###SLACK_BOT_TOKEN###~${SLACK_BOT_TOKEN_DEV}~" config.dev.json
 
 
 jq --arg pk "$DIALOGFLOW_PRIVATE_KEY_PROD" '.DIALOGFLOW_PRIVATE_KEY_PROD = $pk' config.prod.json
 
-sed -i 's~###DIALOGFLOW_CLIENT_EMAIL###~"$DIALOGFLOW_CLIENT_EMAIL_PROD"~' config.prod.json
-sed -i 's~###DIALOGFLOW_PROJECT_ID###~"$DIALOGFLOW_PROJECT_ID_PROD"~' config.prod.json
-sed -i 's~###SLACK_BOT_TOKEN###~"$SLACK_BOT_TOKEN_PROD"~' config.prod.json
+sed -i "s~###DIALOGFLOW_CLIENT_EMAIL###~${DIALOGFLOW_CLIENT_EMAIL_PROD}~" config.prod.json
+sed -i "s~###DIALOGFLOW_PROJECT_ID###~${DIALOGFLOW_PROJECT_ID_PROD}~" config.prod.json
+sed -i "s~###SLACK_BOT_TOKEN###~${SLACK_BOT_TOKEN_PROD}~" config.prod.json
