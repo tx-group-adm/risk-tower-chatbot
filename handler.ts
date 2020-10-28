3354bfecc3f439d81cc904f3608ae9acfb1ca52c
Lukas Schwab <lukas.schwab@bbcag.ch>
2020-10-28 17:34:53 +0100

    make handler use callback

--- handler.ts
+++ handler.ts
@@ -2,15 +2,16 @@ import { HandlerResponse, IEvent, ISlackEventCallback } from './src/interfaces';
 import { slackMessageIMHandler } from './src/handlers/slackMessageIMHandler';
 import { isSlackEvent } from './src/helpers/isSlackEvent';
 import { HTTP200 } from './src/responses';
+import { Callback, Context } from 'aws-lambda';
 
-export const slackevent = (event: IEvent): HandlerResponse => {
+export const slackevent = (event: IEvent, context: Context, callback: Callback): void => {
 	console.log(JSON.stringify(event));
 	if (isSlackEvent(event)) {
 		console.log('EVENT_CALLBACK');
 		const slackEvent: ISlackEventCallback = JSON.parse(event.body);
 		slackMessageIMHandler(slackEvent.event);
-		return HTTP200();
+		return callback(null, HTTP200());
 	} else {
-		return HTTP200('warmup received');
+		return callback(null, HTTP200('warmup received'));
 	}
 };
