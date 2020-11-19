4903bd73148c3fcde3e0fc40c2ce547423ae92aa
Lukas Schwab <lukas.schwab@bbcag.ch>
2020-11-19 01:59:58 +0100

    Add default handler

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -4,6 +4,8 @@ export const INTENTS = {
 	GET_RISKS: 'GetRisks',
 	GET_TOP_FINDINGS: 'GetTopFindings',
 	GET_TOP_MEASURES: 'GetTopMeasures',
+	DEFAULT_WELCOME: 'Default Welcome Intent',
+	DEFAULT_FALLBACK: 'Default Fallback Intent',
 };
 
 export { handleGetAssessmentData } from './handleGetAssessmentData';
