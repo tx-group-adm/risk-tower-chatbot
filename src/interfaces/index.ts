58121ed54e5f924751962e12bb01a3ab092fbfdb
Lukas Schwab <lukas.schwab@bbcag.ch>
2020-11-26 10:04:07 +0100

    Send message as user

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -47,40 +47,42 @@ export interface ISlackMessageIMEvent {
 	upload?: boolean;
 }
 
+export interface ISlackProfile {
+	title: string;
+	phone: string;
+	skype: string;
+	real_name: string;
+	real_name_normalized: string;
+	display_name: string;
+	display_name_normalized: string;
+	fields: {
+		[index: string]: {
+			value: string;
+			alt: string;
+		};
+	};
+	status_text: string;
+	status_emoji: string;
+	status_expiration: number;
+	avatar_hash: string;
+	image_original: string;
+	is_custom_image: boolean;
+	email: string;
+	first_name: string;
+	last_name: string;
+	image_24: string;
+	image_32: string;
+	image_48: string;
+	image_72: string;
+	image_192: string;
+	image_512: string;
+	image_1024: string;
+	status_text_canonical: string;
+}
+
 export interface ISLackProfileResponse {
 	ok: boolean;
-	profile: {
-		title: string;
-		phone: string;
-		skype: string;
-		real_name: string;
-		real_name_normalized: string;
-		display_name: string;
-		display_name_normalized: string;
-		fields: {
-			[index: string]: {
-				value: string;
-				alt: string;
-			};
-		};
-		status_text: string;
-		status_emoji: string;
-		status_expiration: number;
-		avatar_hash: string;
-		image_original: string;
-		is_custom_image: boolean;
-		email: string;
-		first_name: string;
-		last_name: string;
-		image_24: string;
-		image_32: string;
-		image_48: string;
-		image_72: string;
-		image_192: string;
-		image_512: string;
-		image_1024: string;
-		status_text_canonical: string;
-	};
+	profile: ISlackProfile;
 	response_metadata: {
 		scopes: Array<string>;
 		acceptedScopes: Array<string>;
