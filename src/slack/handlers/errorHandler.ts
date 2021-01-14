1e08dcd5aa8bf32d6826a3f835cf6d00283c0931
Lukas Schwab <lukas.schwab@bbcag.ch>
2021-01-14 15:23:00 +0100

    fix typo

--- src/slack/handlers/errorHandler.ts
+++ src/slack/handlers/errorHandler.ts
@@ -2,6 +2,6 @@ import SlackService from '../../services/SlackService';
 
 export async function errorHandler(slackService: SlackService): Promise<void> {
 	const errorMessage =
-		':warning:Oops, something went wrong there!:warning\nWe are trying to fix the problem, please try again later.:hammer_and_wrench:';
+		':warning:Oops, something went wrong there!:warning:\nWe are trying to fix the problem, please try again later.:hammer_and_wrench:';
 	await slackService.postMessage(errorMessage);
 }
