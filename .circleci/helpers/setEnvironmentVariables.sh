8814c4d49313eacb8f305f422806c274009eda01
Lukas Schwab <lukas.schwab@bbcag.ch>
2022-02-24 11:46:01 +0100

    Add okta auth

--- .circleci/helpers/setEnvironmentVariables.sh
+++ .circleci/helpers/setEnvironmentVariables.sh
@@ -8,6 +8,8 @@ jq --arg pk "$DIALOGFLOW_PRIVATE_KEY_DEV" '.DIALOGFLOW_PRIVATE_KEY = $pk' config
 sed -i "s~###DIALOGFLOW_CLIENT_EMAIL###~${DIALOGFLOW_CLIENT_EMAIL_DEV}~" config.dev.json
 sed -i "s~###DIALOGFLOW_PROJECT_ID###~${DIALOGFLOW_PROJECT_ID_DEV}~" config.dev.json
 sed -i "s~###SLACK_BOT_TOKEN###~${SLACK_BOT_TOKEN_DEV}~" config.dev.json
+sed -i "s~###OKTA_CLIENT_ID###~${OKTA_CLIENT_ID_DEV}~" config.dev.json
+sed -i "s~###OKTA_CLIENT_SECRET###~${OKTA_CLIENT_SECRET_DEV}~" config.dev.json
 
 
 jq --arg pk "$DIALOGFLOW_PRIVATE_KEY_PROD" '.DIALOGFLOW_PRIVATE_KEY = $pk' config.prod.json > config.prod.json.tmp && mv config.prod.json.tmp config.prod.json
@@ -15,3 +17,5 @@ jq --arg pk "$DIALOGFLOW_PRIVATE_KEY_PROD" '.DIALOGFLOW_PRIVATE_KEY = $pk' confi
 sed -i "s~###DIALOGFLOW_CLIENT_EMAIL###~${DIALOGFLOW_CLIENT_EMAIL_PROD}~" config.prod.json
 sed -i "s~###DIALOGFLOW_PROJECT_ID###~${DIALOGFLOW_PROJECT_ID_PROD}~" config.prod.json
 sed -i "s~###SLACK_BOT_TOKEN###~${SLACK_BOT_TOKEN_PROD}~" config.prod.json
+sed -i "s~###OKTA_CLIENT_ID###~${OKTA_CLIENT_ID_PROD}~" config.prod.json
+sed -i "s~###OKTA_CLIENT_SECRET###~${OKTA_CLIENT_SECRET_PROD}~" config.prod.json
